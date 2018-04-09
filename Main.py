9018fcb3348d463f0f5ecd58012bc4447712c5a6
Nelis op Laptop <nelisvolschenk@gmail.com>
2018-04-09 14:58:04 +0200

    test with ac power setpoint

--- Main.py
+++ Main.py
@@ -50,7 +50,7 @@ class CCGXController(object):
 
         values = [self.DbusServices['Soc']['Value'], self.DbusServices['L1Power']['Value'],
                   self.DbusServices['L2Power']['Value'], self.DbusServices['L3Power']['Value']]
-        print self.DbusServices['Soc']['Value'] + 1
+        print self.DbusServices['Soc']['Value']
         return values
 
     def setvalues(self,inputpower):
